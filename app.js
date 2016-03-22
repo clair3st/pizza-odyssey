0aa770799efcc32539b43d927554782e17cfc87e
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-03-22 14:35:17 -0700

    created a ballard object to contain pizza data

--- app.js
+++ app.js
@@ -4,7 +4,52 @@ function randomNo (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-//function for string to output depending on the amount of delivery drivers
+//arrays of opening hours
+var openHoursArray = ['8:00am','9:00am','10:00am','11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm,', '7:00pm', '8:00pm', '9:00pm', '10:00pm', '11:00pm', '12:00am', '1:00am'];
+
+//add data to pizzaSold array
+var pizzaSold = [];
+for (var i = 0; i < openHoursArray.length; i++) {
+  if (i < 3) {
+    pizzaSold.push(randomNo(0,4));
+  } else if (i > 2 && i < 6) {
+    pizzaSold.push(randomNo(0,7));
+  } else if (i > 5 && i < 9) {
+    pizzaSold.push(randomNo(2,15));
+  } else if (i > 8 && i < 12) {
+    pizzaSold.push(randomNo(15,35));
+  } else if (i > 11 && i < 15) {
+    pizzaSold.push(randomNo(12,31));
+  } else {
+    pizzaSold.push(randomNo(5,20));
+  }
+}
+console.log(pizzaSold);
+
+//add data to pizzaDelivered Array
+var pizzaDelivered = [];
+for (var i = 0; i < openHoursArray.length; i++) {
+  if (i < 6) {
+    pizzaDelivered.push(randomNo(0,4));
+  } else if (i > 5 && i < 9) {
+    pizzaDelivered.push(randomNo(1,4));
+  } else if (i > 8 && i < 12) {
+    pizzaDelivered.push(randomNo(5,20));
+  } else if (i > 11 && i < 15) {
+    pizzaDelivered.push(randomNo(5,12));
+  } else {
+    pizzaDelivered.push(randomNo(6,11));
+  }
+}
+console.log(pizzaDelivered);
+
+//Array of delivery drivers
+var deliveryDriversRequired = [];
+for (var i = 0; i < openHoursArray.length; i++) {
+  deliveryDriversRequired.push(Math.ceil(pizzaDelivered[i] / 3));
+}
+
+//Add string depending on drivers
 function deliveryString (drivers) {
   if (drivers === 0) {
     return ' -- [driver not recommended ]';
@@ -13,51 +58,33 @@ function deliveryString (drivers) {
   }
 }
 
-//Object containing time of day, pizza's sold, pizza's deliveried and drivers needed.
-var timeDeliveryPizza = function(time, minDelivery, maxDelivery, minPizza, maxPizza) {
-  this.timeOfDay = time;
-  this.pizzaSold = randomNo(minPizza, maxPizza);
-  this.pizzaDelivered = randomNo(minDelivery, maxDelivery);
-  this.deliveryDrivers = Math.ceil(this.pizzaDelivered / 3);
-  this.deliveryDriversString = deliveryString(this.deliveryDrivers);
-};
-
-//Store data for each time in a variable using the timeDeliveryPizza object constructor
-var eightAM = new timeDeliveryPizza ('8:00am', 0, 4, 0, 4);
-var nineAM = new timeDeliveryPizza ('9:00am', 0, 4, 0, 4);
-var tenAM = new timeDeliveryPizza ('10:00am', 0, 4, 0, 4);
-var elevenAM = new timeDeliveryPizza ('11:00am', 0, 4, 0, 7);
-var twelvePM = new timeDeliveryPizza ('12:00pm', 0, 4, 0, 7);
-var onePM = new timeDeliveryPizza ('1:00pm', 0, 4, 0, 7);
-var twoPM = new timeDeliveryPizza ('2:00pm', 1, 4, 2, 15);
-var threePM = new timeDeliveryPizza ('3:00pm', 1, 4, 2, 15);
-var fourPM = new timeDeliveryPizza ('4:00pm', 1, 4, 2, 15);
-var fivePM = new timeDeliveryPizza ('5:00pm', 3, 8, 15, 35);
-var sixPM = new timeDeliveryPizza ('6:00pm', 3, 8, 15, 35);
-var sevenPM = new timeDeliveryPizza ('7:00pm', 3, 8, 15, 35);
-var eightPM = new timeDeliveryPizza ('8:00pm', 5, 12, 12, 31);
-var ninePM = new timeDeliveryPizza ('9:00pm', 5, 12, 12, 31);
-var tenPM = new timeDeliveryPizza ('10:00pm', 5, 12, 12, 31);
-var elevenPM = new timeDeliveryPizza ('11:00pm', 6, 11, 5, 20);
-var twelveAM = new timeDeliveryPizza ('12:00pm', 6, 11, 5, 20);
-var oneAM = new timeDeliveryPizza ('1:00pm', 6, 11, 5, 20);
-
-//Variables for each time of day is stored in an array.
-var hoursOfTheDayArray = [eightAM, nineAM, tenAM, elevenAM, twelvePM, onePM, twoPM, threePM, fourPM, fivePM, sixPM, sevenPM, eightPM, ninePM, tenPM, elevenPM, twelveAM, oneAM];
-
-//function to display each hour on DOM as a bullet point
-function DOMdisplay(htmlID) {
-  for (var i = 0; i < hoursOfTheDayArray.length; i++) {
-    var bodyHeader = document.getElementById(htmlID);
-    var pageHeading = document.createElement('li');
-    pageHeading.textContent = hoursOfTheDayArray[i].timeOfDay + ' ' + hoursOfTheDayArray[i].pizzaSold + ' pizzas, ' + hoursOfTheDayArray[i].pizzaDelivered + hoursOfTheDayArray[i].deliveryDriversString;
-    bodyHeader.appendChild(pageHeading);
-  }
+//add data to salesDataArray which is objects of sales data for each time.
+var salesDataArray = [];
+for (var i = 0; i < openHoursArray.length; i++) {
+  salesDataArray.push({
+    time: openHoursArray[i],
+    pizzas: pizzaSold[i],
+    delivery: pizzaDelivered[i],
+    deliveryDriver: deliveryDriversRequired[i],
+    deliveryString: deliveryString(deliveryDriversRequired[i]),
+  });
 }
+console.log(salesDataArray);
 
-DOMdisplay('ballardList');
-DOMdisplay('firstHillList');
-DOMdisplay('intDistrictList');
-DOMdisplay('sthLakeUnion');
-DOMdisplay('georgetownList');
-DOMdisplay('ravennaList');
+//Create Ballard Object
+var ballard = {
+  name: 'Ballard',
+  storeData: salesDataArray,
+};
+console.log(ballard.name);
+
+//Try to print to DOM.
+var ballardUL = document.getElementById('ballardList');
+var ballardULlist = document.createElement('li');
+ballardULlist.textContent = 'ballard.name';
+ballardULlist.appendChild = ballardULlist;
+/*
+var eightOclockLi;
+for (var i = 0; i < ballard.hourlyData.length; i++) {
+  eigthOclockLi = document.createElement('li');
+  eightOclockLi.textContent =*/
